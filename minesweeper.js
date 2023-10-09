399f79581b99cb4d49efc0c484fbb3675328ba1b
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2023-10-09 10:26:02 +0200

    mine variable created

--- minesweeper.js
+++ minesweeper.js
@@ -7,11 +7,12 @@ let x = 0;
 let y = 0;
 const columns = canvas.width / size;
 const rows = canvas.height / size;
+const mine = "mine";
 
 let map = [
-  [1, 0, 0, 0, 0],
+  [1, mine, 0, 0, 0],
   [0, 0, 1, 0, 1],
-  [1, 0, 0, 1, 0],
+  [1, 0, 0, 1,mine],
 ]
 
 console.log(map);
